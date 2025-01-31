7d1203b70f9f88f467923d70376a175b235ebf7a
GailCodes <xross_dev@proton.me>
2025-01-30 22:42:46 -0800

    Add rainbow effect

--- terminal_greeting.py
+++ terminal_greeting.py
@@ -1,8 +1,17 @@
 import random
 import json
+import time
+from colorama import Fore, init
+
+
+# Initialize colorama
+init(autoreset=True)
 
 greetings = None
+colors = [Fore.YELLOW, Fore.GREEN, Fore.CYAN, Fore.BLUE, Fore.MAGENTA]
 
+
+# Get greetings from file
 try:
     with open("greetings.json", "r") as file:
         greetings = json.load(file)
@@ -11,5 +20,16 @@ except FileNotFoundError:
     exit
 
 
-# Print the greeting
-print(greetings[random.randint(0, len(greetings) - 1)])
+selected_greeting = greetings[random.randint(0, len(greetings) - 1)]
+
+
+# Print the greeting with a rainbow effect
+for i, char in enumerate(selected_greeting):
+    color = colors[i % len(colors)]
+    print(color + char, end="", flush=True)
+    time.sleep(0.03)
+
+
+# Print a newline so terminal doesn't look weird
+print("\n", end="")
+
