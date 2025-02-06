5e2d0b2020b5bd1cd9effc8da36f495d28a9a04b
GailCodes <xross_dev@proton.me>
2025-02-06 12:56:41 -0800

    add main function and rainbowText refactor

--- term_greeter.py
+++ term_greeter.py
@@ -11,26 +11,33 @@ greetings = None
 colors = [Fore.YELLOW, Fore.GREEN, Fore.CYAN, Fore.BLUE, Fore.MAGENTA]
 
 
-# Get greetings from file
-try:
-    with open("greetings.json", "r") as file:
-        greetings = json.load(file)
-except FileNotFoundError:
-    print("The greetings file does not exist")
-    exit
+def main():
+    # Get greetings from file
+    try:
+        with open("greetings.json", "r") as file:
+            greetings = json.load(file)
+    except FileNotFoundError:
+        print("The greetings file does not exist")
+        exit
 
 
-# Get random greeting from the list of greetings
-selected_greeting = greetings[random.randint(0, len(greetings) - 1)]
+    # Get random greeting from the list of greetings
+    selected_greeting = greetings[random.randint(0, len(greetings) - 1)]
 
 
-# Print the greeting with a rainbow effect
-for i, char in enumerate(selected_greeting):
-    color = colors[i % len(colors)]
-    print(color + char, end="", flush=True)
-    time.sleep(0.03)
+    printRainbowText(selected_greeting)
 
 
-# Print a newline so terminal doesn't look weird
-print("\n", end="")
+    # Print a newline so terminal doesn't look weird
+    print("\n", end="")
 
+
+def printRainbowText(text):
+    # Print the greeting with a rainbow effect
+    for i, char in enumerate(text):
+        color = colors[i % len(colors)]
+        print(color + char, end="", flush=True)
+        time.sleep(0.03)
+
+if __name__ == "__main__":
+    main()
